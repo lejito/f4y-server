bd1bc3b5057d46c3cbdf34f6a42add9681920ea8
Adrian David Perdomo Echeverri <84606566+AdrianPerdomoE@users.noreply.github.com>
2024-03-03 14:13:41 -0500

    Update: analisis de codigo del middleware validator

--- src/middlewares/validator.middleware.js
+++ src/middlewares/validator.middleware.js
@@ -7,33 +7,53 @@ module.exports = (requiredParams) => {
    * @param {import('express').NextFunction} next
    **/
   return (req, res, next) => {
+    //<1>
     const missingParams = [];
     const invalidParams = [];
     const nullParams = [];
-
+    //<1>
+    //<2>
     for (const param of requiredParams) {
+      //<3>
       if (!(param.name in req.body)) {
+        //<4>
         missingParams.push(param);
-      } else if (!param.nullable) {
+        //<4>
+      }
+      //<3>
+      //<5>
+      else if (!param.nullable) {
+        //<6>
         if (
           req.body[param.name] === null ||
           req.body[param.name] === undefined
         ) {
+          //<7>
           nullParams.push(param);
-        } else if (
+          //<7>
+        } 
+        //<6>
+        //<8>
+        else if (
           !param.nullable &&
           !(typeof req.body[param.name] === param.type)
         ) {
+          //<9>
           invalidParams.push(param);
+          //<9>
         }
+        //<8>
       }
+      //<5>
     }
-
+    //<2>
+    //<10>
     if (
       missingParams.length > 0 ||
       invalidParams.length > 0 ||
       nullParams.length > 0
     ) {
+      //<11>
       return res.status(400).json(
         utils.errorResponse(
           "Los parámetros proporcionados no son los suficientes, no son del tipo válido o son nulos.",
@@ -44,7 +64,10 @@ module.exports = (requiredParams) => {
           }
         )
       );
-    } else {
+      //<11>
+    } 
+    //<10>
+    else {
       return next();
     }
   };
